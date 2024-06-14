7698db6d6ff5f3634fe05f19ac807d2a787e9c98
Chris Ware <c.p.ware@hotmail.com>
2024-06-13 17:49:09 -0700

    Episode 3

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,12 +1,13 @@
 import Hero from "@/components/Frontend/Hero";
 import MegaMenu from "@/components/Frontend/MegaMenu";
+import TabbedSection from "@/components/Frontend/TabbedSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      
       <Hero />
+      <TabbedSection />
     </div>
   );
 }
